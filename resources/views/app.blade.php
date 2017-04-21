c9ffb50f7c3353d5affd2b15a1bdac6bfa96f6e2
flamez <1050355217@qq.com>
2017-04-21 10:14:58 +0800

    修改样式路径错误

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,23 +14,23 @@
     <meta content="@yield('description')" name="description" />
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <!-- <link href="http://fonts.useso.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" /> -->
-    <link href="{{ asset('static') }}/metronic/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('static') }}/metronic/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('static') }}/metronic/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('static') }}/metronic/global/plugins/flatui/css/flat-ui.min.css" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('static') }}/metronic/global/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('static') }}/metronic/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('vendor') }}/metronic/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('vendor') }}/metronic/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('vendor') }}/metronic/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('vendor') }}/metronic/global/plugins/flatui/css/flat-ui.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('vendor') }}/metronic/global/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('vendor') }}/metronic/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
     <!-- END GLOBAL MANDATORY STYLES -->
     <!-- BEGIN PAGE LEVEL PLUGINS -->
-    <link href="{{ asset('static') }}/metronic/global/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('static') }}/metronic/global/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('vendor') }}/metronic/global/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('vendor') }}/metronic/global/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
     <!-- END PAGE LEVEL PLUGINS -->
     <!-- BEGIN THEME GLOBAL STYLES -->
-    <link href="{{ asset('static') }}/metronic/global/css/components-md.min.css" rel="stylesheet" id="style_components" type="text/css" />
-    <link href="{{ asset('static') }}/metronic/global/css/plugins-md.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('vendor') }}/metronic/global/css/components-md.min.css" rel="stylesheet" id="style_components" type="text/css" />
+    <link href="{{ asset('vendor') }}/metronic/global/css/plugins-md.min.css" rel="stylesheet" type="text/css" />
     <!-- END THEME GLOBAL STYLES -->
     <!-- BEGIN PAGE LEVEL STYLES -->
-    <link href="{{ asset('static') }}/metronic/pages/css/login-2.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('vendor') }}/metronic/pages/css/login-2.min.css" rel="stylesheet" type="text/css" />
     <!-- END PAGE LEVEL STYLES -->
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" />
     @yield('script')
